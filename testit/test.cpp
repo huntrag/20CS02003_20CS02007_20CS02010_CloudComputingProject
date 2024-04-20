bfa80b0f6cf44480093b6190113c19ab0eb5bed5
Raghav Gade <r29aghavgade@gmail.com>
2024-04-20 19:42:45 +0530

    Run threads like this

---
+++ testit/test.cpp
@@ -0,0 +1,20 @@
+#include <thread>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+using namespace std;
+
+void print(int x)
+{
+    cout << x << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
